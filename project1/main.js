7d2751475448632ad4397f434aa785e451330972
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2022-03-02 20:34:41 +0530

    Update main.js

--- project1/main.js
+++ project1/main.js
@@ -5,8 +5,11 @@ const sub = require("./subtract.js") ;
 const mul = require("./multiply.js") ; 
 const div = require("./divide.js") ; 
 
-console.log("adding =",add(2,5)) ;    
-console.log("subtract =",sub(10,5)) ;     
-console.log("multiply =",mul(2,4)) ;    
-console.log("divide =",div(10,5)) ;        
+var a = 10 ;
+var b = 2 ; 
+
+console.log("adding",add(a,b)) ;    
+console.log("subtract =",sub(a,b)) ;     
+console.log("multiply =",mul(a,b)) ;    
+console.log("divide =",div(a,b)) ;        
 
