81a30e7a80a9e9dd0f2633303d958410ac3d7a10
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2022-03-07 12:53:11 +0530

    unit 4 part3

--- project2/index.js
+++ project2/index.js
@@ -5,16 +5,15 @@ const app = express();
 const books = require('./data')
 
 app.get("/Home" ,function (request,response){
-    response.send("Hello");
-});
+    response.send("Hello");     
+});    
 
 app.get("/books" ,function (request,response){
-    // response.send("Hello Books");
+    // response.send("Hello Books"); 
     response.send(books);
 });
 
 app.listen(4050,() => {
-
-    console.log("listeining on port 4050");
+    console.log("listeining on port 4050");   
 });
 
